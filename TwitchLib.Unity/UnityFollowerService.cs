577880b18293e4917abc175a179edc3e876bc205
Björn Lindholm <bjorn.lindholm@bmgdesign.net>
2018-03-11 18:45:58 +0200

    Renamed TwitchPubSubUnity to PubSub. Added UnityFollower Service. Added UnityLiveStreamMonitor.

---
+++ TwitchLib.Unity/UnityFollowerService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using UnityEngine;
+using TwitchLib.Api;
+using TwitchLib.Api.Enums;
+using TwitchLib.Api.Interfaces;
+using TwitchLib.Api.Services;
+using TwitchLib.Api.Services.Exceptions;
+using TwitchLib.Api.Services.Events.FollowerService;
+
+namespace TwitchLib.Unity {
+    public class UnityFollowerService : FollowerService {
+        private readonly GameObject _threadDispatcher;
+
+        #region Events
+        /// <summary>EventHandler for named event.</summary>
+        public new event EventHandler<OnServiceStartedArgs> OnServiceStarted;
+        /// <summary>EventHandler for named event.</summary>
+        public new event EventHandler<OnServiceStoppedArgs> OnServiceStopped;
+        /// <summary>EventHandler for named event.</summary>
+        public new event EventHandler<OnNewFollowersDetectedArgs> OnNewFollowersDetected;
+        #endregion
+
+        public UnityFollowerService(ITwitchAPI api, int checkIntervalSeconds = 60, int queryCount = 25) : base(api, checkIntervalSeconds, queryCount) {
+            _threadDispatcher = new GameObject("ThreadDispatcher");
+            _threadDispatcher.AddComponent<ThreadDispatcher>();
+            UnityEngine.Object.DontDestroyOnLoad(_threadDispatcher);
+
+            base.OnServiceStarted += ((object sender, OnServiceStartedArgs e) => { ThreadDispatcher.Instance().Enqueue(() => OnServiceStarted?.Invoke(sender, e)); });
+            base.OnServiceStopped += ((object sender, OnServiceStoppedArgs e) => { ThreadDispatcher.Instance().Enqueue(() => OnServiceStopped?.Invoke(sender, e)); });
+            base.OnNewFollowersDetected += ((object sender, OnNewFollowersDetectedArgs e) => { ThreadDispatcher.Instance().Enqueue(() => OnNewFollowersDetected?.Invoke(sender, e)); });
+        }
+    }
+}
