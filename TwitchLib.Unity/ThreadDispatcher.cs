b453d3e1739b0dfca471c074f580ffd52ff1ac8d
JohnGames <37758871+JohnGames@users.noreply.github.com>
2019-10-30 17:21:36 -0400

    Stability Fixes and Package Updates (#50)
    * Sync
    
    * Sync
    
    * Set to use latest preview builds on Nuget.
    
    * Fix thread dispatcher creating itself when not in play mode for Unity Editor.
    Upgrade Api from 3.0.6 to 3.1.2
    Upgrade Client from 3.0.6 to 3.1.4
    Upgrade PubSub from 3.0.6 to 3.1.3

--- TwitchLib.Unity/ThreadDispatcher.cs
+++ TwitchLib.Unity/ThreadDispatcher.cs
@@ -5,46 +5,48 @@ using UnityEngine;
 
 namespace TwitchLib.Unity
 {
-    public class ThreadDispatcher : MonoBehaviour
-    {
-        private static ThreadDispatcher _instance;
+	public class ThreadDispatcher : MonoBehaviour
+	{
+		private static ThreadDispatcher _instance;
 
-        private static Queue<Action> _executionQueue = new Queue<Action>();
+		private static Queue<Action> _executionQueue = new Queue<Action>();
 
-        /// <summary>
-        /// Ensures a thread dispatcher is created if there is none.
-        /// </summary>
-        public static void EnsureCreated([CallerMemberName] string callerMemberName = null)
-        {
-            if (_instance == null || _instance.gameObject == null)
-                _instance = CreateThreadDispatcherSingleton(callerMemberName);
-        }
+		/// <summary>
+		/// Ensures a thread dispatcher is created if there is none.
+		/// </summary>
+		public static void EnsureCreated([CallerMemberName] string callerMemberName = null)
+		{
+			if (!Application.isPlaying)
+				return;
+			if (_instance == null || _instance.gameObject == null)
+				_instance = CreateThreadDispatcherSingleton(callerMemberName);
+		}
 
-        /// <summary>
-        /// Locks the queue and adds the Action to the queue
-        /// </summary>
-        /// <param name="action">function that will be executed from the main thread.</param>
-        public static void Enqueue(Action action)
-        {
-            _executionQueue.Enqueue(action);
-        }
+		/// <summary>
+		/// Locks the queue and adds the Action to the queue
+		/// </summary>
+		/// <param name="action">function that will be executed from the main thread.</param>
+		public static void Enqueue(Action action)
+		{
+			_executionQueue.Enqueue(action);
+		}
 
-        private void Update()
-        {
-            //storing the count here instead of locking the queue so we don't end up with a deadlock when one of the actions queues another action
-            int count = _executionQueue.Count;
-            for (int i = 0; i < count; i++)
-                _executionQueue.Dequeue().Invoke();
-        }
+		private void Update()
+		{
+			//storing the count here instead of locking the queue so we don't end up with a deadlock when one of the actions queues another action
+			int count = _executionQueue.Count;
+			for (int i = 0; i < count; i++)
+				_executionQueue.Dequeue().Invoke();
+		}
 
-        private static ThreadDispatcher CreateThreadDispatcherSingleton(string callerMemberName)
-        {
-            if (!UnityThread.CurrentIsMainThread)
-                throw new InvalidOperationException($"The {callerMemberName} can only be created from the main thread. Did you accidentally delete the " + nameof(ThreadDispatcher) + " in your scene?");
+		private static ThreadDispatcher CreateThreadDispatcherSingleton(string callerMemberName)
+		{
+			if (!UnityThread.CurrentIsMainThread)
+				throw new InvalidOperationException($"The {callerMemberName} can only be created from the main thread. Did you accidentally delete the " + nameof(ThreadDispatcher) + " in your scene?");
 
-            ThreadDispatcher threadDispatcher = new GameObject(nameof(ThreadDispatcher)).AddComponent<ThreadDispatcher>();
-            DontDestroyOnLoad(threadDispatcher);
-            return threadDispatcher;
-        }
-    }
+			ThreadDispatcher threadDispatcher = new GameObject(nameof(ThreadDispatcher)).AddComponent<ThreadDispatcher>();
+			DontDestroyOnLoad(threadDispatcher);
+			return threadDispatcher;
+		}
+	}
 }
